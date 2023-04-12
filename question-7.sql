9c35e47e5c7ffe3456563c9770ad78d3a9e5a21a
tshres <tshres@uw.edu>
2023-04-11 22:29:11 -0700

    extra credit attempt

--- question-7.sql
+++ question-7.sql
@@ -1,4 +1,7 @@
 -- Find tracks (id, name, and composer) that have never been purchased (that is, they aren't part of a line item in an invoice).
 SELECT tracks.TrackId, tracks.Name, tracks.Composer
 FROM tracks
-JOIN invoice_items ON tracks.TrackId = invoice_items.TrackId;
+LEFT JOIN invoice_items ON tracks.TrackId = invoice_items.TrackId
+LEFT JOIN invoices ON invoice_items.InvoiceId = invoices.InvoiceId
+LEFT JOIN customers ON invoices.CustomerId = customers.CustomerId
+WHERE invoice_items.InvoiceLineId IS NULL;
